6a42be87e47af63682b38a24618e9dfca9b2a890
pratyaksh64 <60766667+pratyaksh64@users.noreply.github.com>
2020-09-30 21:26:37 +0530

    Update mask_recog.py
    commit 2

--- mask_recog.py
+++ mask_recog.py
@@ -1,9 +1,13 @@
 import cv2  
 import dlib 
 import pyttsx3
+import playsound
 
 engine = pyttsx3.init()
-
+# =============================================================================
+# engine.say('wear a mask')
+# engine.runandwait()
+# =============================================================================
 cap = cv2.VideoCapture(0) 
 detector = dlib.get_frontal_face_detector() 
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
@@ -13,7 +17,7 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = detector(gray) 
     for face in faces: 
-        x1 = face.left() 
+        x1 = face.left()
         y1 = face.top() 
         x2 = face.right() 
         y2 = face.bottom() 
@@ -24,10 +28,15 @@ while True:
             x = landmarks.part(n).x 
             y = landmarks.part(n).y 
             #cv2.rectangle(frame, (x, y), (x+5, y+5), (0, 255, 255))
-            t = cv2.circle(frame, (x, y), 2, (255, 255, 0), -1) 
-            if t.any()== True:
-                engine.say('wear a mask')
-                print('wear mask')
+            t = cv2.circle(frame, (x, y), 2, (255, 255, 0), -1)
+            if t.any()==True:
+              engine.say('wear a mask')
+              engine.runAndWait()
+              break
+
+#                playsound.playsound('audio.mp3')
+#                break
+            print('wear mask')
     cv2.imshow("Frame", frame) 
     key = cv2.waitKey(1)
     if key == 27: 
